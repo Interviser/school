d72eed19bfb76142e97ae8e590977da4d0a07a37
Interviser <153377810+Interviser@users.noreply.github.com>
2026-01-07 00:52:38 -0800

    updated

--- server/controller/registerAdmin.js
+++ server/controller/registerAdmin.js
@@ -1,36 +1,11 @@
 const bcrypt = require('bcrypt');
-
-const verifyToken = (req,res,next)=>{
-    try{
-        const token = req.headers['authorization'].split(" ")[1]
-        console.log('token sent')
-        if(!token){
-            return res.status(403).json({messagae: 'no token provided'})
-        }
-         jwt.verify(
-            token,
-            process.env.JWT_SECRET_KEY,
-        (err,decoded)=>{
-            if(err){
-                return res.status(404).json("cannot verify")
-            }
-            req.id =decoded.id
-        })
-      
-           
-        next();
-    }
-    catch(err){
-       return console.log("an error occurred")
-    }
-}
-
+const sanitize = require('mongo-sanitize');
 
 const students_model = require('../model/signup_model');
 
-const signUpadmin_controller = (verifyToken, async (req,res)=>{
+const signUpadmin_controller = ( async (req,res)=>{
     try{
-        const studentsData = new students_model(req.body)
+        const studentsData = new students_model(sanitize(req.body))
         const {firstName, lastName,emailAddress,password, guardianName, contact} = studentsData;
         if(!firstName || !lastName || !emailAddress || !password || !guardianName || !contact){
             return res.status(400).json("all fields are required")
@@ -45,12 +20,13 @@ const signUpadmin_controller = (verifyToken, async (req,res)=>{
        if(password.length < 6){
         return res.status(400).json("password must be at least 6 characters")
        }
-       const no_of_records = await students_model.countDocuments();
-       const id = `CCIS/26/${no_of_records+1}`;
+       const bool = sanitize(true);
+       const no_of_records = await students_model.countDocuments({"admin":bool});
+       const id = `CCIS/staff/${no_of_records+1}`;
        const role = true
        studentsData.id = id;
        studentsData.admin = role;
-       const hashedPassword = await bcrypt.hash(password,14);
+       const hashedPassword = await bcrypt.hash(password,12);
        studentsData.password = hashedPassword;
 
        
--- server/controller/signup.js
+++ server/controller/signup.js
@@ -1,11 +1,10 @@
  const bcrypt = require('bcrypt');
 require('dotenv').config();
-
-const saltRounds= process.env.SALT_ROUNDS;
+const sanitize = require('mongo-sanitize');
 const students_model = require('../model/signup_model');
 
 const signUp_controller =  async (req,res) =>{
-      const studentsData = new students_model(req.body)
+      const studentsData = new students_model(sanitize(req.body))
         const {firstName, lastName,emailAddress,password, guardianName, contact} = studentsData;
         if(!firstName || !lastName || !emailAddress || !password || !guardianName || !contact){
             return res.status(400).json("all fields are required")
@@ -20,10 +19,11 @@ const signUp_controller =  async (req,res) =>{
        if(password.length < 6){
         return res.status(400).json("password must be at least 6 characters")
        }
-       const no_of_records = await students_model.countDocuments();
+       const bool =sanitize(false);
+       const no_of_records = await students_model.countDocuments({"admin":bool});
        const generated_id = `CCIS/26/${no_of_records+1}`;
        studentsData.id = generated_id;
-       const hashedPassword = await bcrypt.hash(password,14);
+       const hashedPassword = await bcrypt.hash(password,12);
        studentsData.password = hashedPassword;
       await studentsData.save();
        res.status(200).json("signed up successfully");
