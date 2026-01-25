944c07644604adfe7e45489dddca29cf59150186
Interviser <153377810+Interviser@users.noreply.github.com>
2026-01-25 02:37:05 -0800

    made middlename optional

--- middleware/dto.js
+++ middleware/dto.js
@@ -2,7 +2,7 @@ const joi = require('joi');
 const signupDTO= joi.object({
    
                 firstName: joi.string().min(2).max(30).required(),
-                middleName: joi.string().min(2).max(30),
+                middleName: joi.string().min(2).max(30).optional().allow(''),
                 lastName: joi.string().min(2).max(30).required(),
                 emailAddress: joi.string().email().required(),
                 password: joi.string().min(6).required(),
